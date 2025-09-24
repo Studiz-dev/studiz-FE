636976a7dab53699967e5b0a2a9c101641fbb8fd
Yea <dbs6463@gmail.com>
2025-09-24 14:08:09 +0900

    메인페이지 tailwind 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { Routes, Route } from 'react-router-dom'
-import MainPage from './pages/MainPage.tsx'
-import DonePage from './pages/DonePage.tsx'
-import GroupPage from './pages/GroupPage.tsx'
-import HomePage from './pages/HomePage.tsx'
-import MyPage from './pages/MyPage.tsx'
-import Sign1Page from './pages/Sign1Page.tsx'
-import Sign2Page from './pages/Sign2Page.tsx'
-import './index.css' 
+import MainPage from './pages/MainPage'
+import DonePage from './pages/DonePage'
+import GroupPage from './pages/GroupPage'
+import HomePage from './pages/HomePage'
+import MyPage from './pages/MyPage'
+import Sign1Page from './pages/Sign1Page'
+import Sign2Page from './pages/Sign2Page'
+import './index.css'
 
 export default function App() {
   return (
-    <div className="mobile-frame scale-80">
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/done" element={<DonePage />} />
-        <Route path="/group" element={<GroupPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/my" element={<MyPage />} />
-        <Route path="/sign1" element={<Sign1Page />} />
-        <Route path="/sign2" element={<Sign2Page />} />
-        <Route path="*" element={<div>404</div>} />
-      </Routes>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="mobile-frame">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/done" element={<DonePage />} />
+          <Route path="/group" element={<GroupPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/my" element={<MyPage />} />
+          <Route path="/sign1" element={<Sign1Page />} />
+          <Route path="/sign2" element={<Sign2Page />} />
+          <Route path="*" element={<div>404</div>} />
+        </Routes>
+      </div>
     </div>
   )
 }
